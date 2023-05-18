f35f7f9f1ef83bbaf702ac52a8cbb3b2d2330ef9
Bun <mbunalski@gmail.com>
2023-05-18 13:46:22 -0400

    Got the DB models setup

--- time_tracker/apps.py
+++ time_tracker/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TrackWebAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'track_web_app'
+    name = 'time_tracker'
--- web_app/settings.py
+++ web_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'track_web_app.apps.TrackWebAppConfig',
+    'time_tracker.apps.TrackWebAppConfig',
 ]
 
 MIDDLEWARE = [
