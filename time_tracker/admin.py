6d7486377850794b62be490526ae9416c7c24060
Bun <mbunalski@gmail.com>
2023-05-19 11:41:19 -0400

    Added some stuff to the database and modified the title given

--- time_tracker/admin.py
+++ time_tracker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import shift_event
+
+admin.site.register(shift_event)
+
 # Register your models here.
--- time_tracker/models.py
+++ time_tracker/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class shift_event(models.Model):
     type_activity = models.CharField(max_length=20)
     time_activity =  models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.type_activity) + " " + str(self.created)
